63559504187799cd6a41ea5d00988d8fdddeacd4
Zachary Jones <zachary@thorough.company>
2018-05-31 09:34:40 -0400

    Initial commit.

---
+++ aliases.zsh
@@ -0,0 +1,10 @@
+# I use the Insiders build of VS Code as my daily driver but its binary is
+# called `code-insiders`, which is really long. So I alias the shorter
+# `code` to it.
+alias code="code-insiders"
+
+# Shortcut for opening my ZSH config in VS Code.
+alias zshconfig="open ~/.oh-my-zsh/custom/Workspace.code-workspace"
+
+# Use GitHub's `git` wrapper
+alias git="hub"
---
+++ brew-extensions.zsh
@@ -0,0 +1,3 @@
+# Add ZSH Autosuggestions.
+# NOTE: If this errors you likely need to install it with `brew`.
+source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
