fcf667c06a343a189d9437d4b355a6b9a23acc7f
Zachary Jones <me@zacharytamas.com>
2018-06-07 13:49:17 -0400

    Add shortcut for the Tower git client.

--- aliases.zsh
+++ aliases.zsh
@@ -6,8 +6,16 @@ alias code="code-insiders"
 # Shortcut for opening my ZSH config in VS Code.
 alias zshconfig="open ~/.oh-my-zsh/custom/Workspace.code-workspace"
 
+################################################################
+# Git shortcuts
+################################################################
+
 # Use GitHub's `git` wrapper
 alias git="hub"
+
+# For when I want a GUI for Git I use Tower. This just makes it easy to
+# open the current directory in Tower.
+alias tower="gittower ."
 
 ################################################################
 # VSCode shortcuts
