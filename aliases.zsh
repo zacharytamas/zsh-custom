d12b4049e478c34763b27aed820fc0511cb4e245
Zachary Jones <me@zacharytamas.com>
2018-09-12 08:40:14 -0400

    Remove alias to Insiders build of VS Code.
    I am using the stable version now.

--- aliases.zsh
+++ aliases.zsh
@@ -1,8 +1,3 @@
-# I use the Insiders build of VS Code as my daily driver but its binary is
-# called `code-insiders`, which is really long. So I alias the shorter
-# `code` to it.
-alias code="code-insiders"
-
 # Shortcut for opening my ZSH config in VS Code.
 alias zshconfig="open ~/.oh-my-zsh/custom/Workspace.code-workspace"
 
